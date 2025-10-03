94d1bf6029e6c8dc689d86420a9868b773fc3379
Yasser Mohammad <yasserfarouk@gmail.com>
2025-10-03 18:23:03 +0900

    extend all

--- convertall.sh
+++ convertall.sh
@@ -3,13 +3,14 @@
 source .venv/bin/activate
 for b in training testing; do
 	for f in environment/scenarios/${b}_src/*; do
-    if [ "$(basename "$f")" = "scml_dynamic" ] || [ "$(basename "$f")" = "anac" ] || [ "$(basename "$f")" = "anac2024" ]; then
-				echo "Converting $f with --extend"
-        python convert_scenarios.py "${f}" --extend
-    else
-				echo "Converting $f without extension"
-        python convert_scenarios.py "${f}"
-    fi
+			python convert_scenarios.py "${f}" --extend
+    # if [ "$(basename "$f")" = "scml_dynamic" ] || [ "$(basename "$f")" = "anac" ] || [ "$(basename "$f")" = "anac2024" ]; then
+    # 				echo "Converting $f with --extend"
+    #     python convert_scenarios.py "${f}" --extend
+    # else
+    # 				echo "Converting $f without extension"
+    #     python convert_scenarios.py "${f}"
+    # fi
 	done
 done
 
