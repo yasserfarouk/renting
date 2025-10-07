3a1ddd038269aacac404601a6e8a92ba0edd43f5
Yasser Mohammad <yasserfarouk@gmail.com>
2025-10-07 16:19:02 +0900

    no anac

--- runallrand.sh
+++ runallrand.sh
@@ -2,6 +2,9 @@
 
 source .venv/bin/activate
 for f in $(ls environment/scenarios/training | shuf); do
+	if [[ $(basename "$f") == anac* ]]; then
+      continue
+	fi
 	echo "Training on $(basename "$f")"
 	python ppo.py --exp=$(basename "$f") $@
 	echo "Testing on $(basename "$f")"
